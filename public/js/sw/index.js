9e1dba70b18af24f742d8965c14bfe9b89d48178
Nicole Freed <nicole.freed@gmail.com>
2018-07-23 00:15:47 -0700

    add custom response to hijack all network requests

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -3,4 +3,12 @@ self.addEventListener('fetch', function(event) {
 	// containing an element with class="a-winner-is-me".
 	// Ensure the Content-Type of the response is "text/html"
   console.log(event.request);
+  event.respondWith(
+    new Response('<h1 class="a-winner-is-me">Request hijacked!</h1>',{
+      headers: {
+        'Content-Type': 'text/html',
+        'header2': 'value2'
+      }
+    })
+  );
 });
